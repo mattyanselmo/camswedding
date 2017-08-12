6fe6429c046e3e814aefb1cbd0e0de12d55e647a
Lechner <Stephanie.Lechner@nike.com>
2017-08-12 14:58:53 -0700

    Working version for wedding.

--- server.R
+++ server.R
@@ -11,31 +11,33 @@ shinyServer(function(input, output) {
    if(input$score == 'Utility'){
      if(!input$projection){
        plot(plotdata$Year, plotdata$UtScore,
-            type = 'l', lwd = 2, col = 'green', xlim = c(2005, 2017), ylim = c(-10, 10),
+            type = 'l', lwd = 3, col = 'blue', xlim = c(2005, 2017), ylim = c(-6, 6),
             main = "Cam's Utility Score", xlab = 'Year', ylab = 'Utility')
      } else{
        plot(plotdata$Year[plotdata$Year < 2050], plotdata$UtScore[plotdata$Year < 2050],
-            type = 'l', lwd = 2, col = 'green', xlim = c(2005, 2050), ylim = c(-10, 50),
+            type = 'l', lwd = 3, col = 'blue', xlim = c(2005, 2050), ylim = c(-5, 50),
             main = "Cam's Utility Score", xlab = 'Year', ylab = 'Utility')
-       lines(c(2017, 2050), c(plotdata$Utscore[plotdata$Year == 2017], plotdata$Utscore[plotdata$Year == 2017] + 45),
-             col = 'green', lwd = 2, lty = 2)
+       lines(c(2017, 2050), c(plotdata$UtScore[plotdata$Year == 2017], plotdata$UtScore[plotdata$Year == 2017] + 45),
+             col = 'blue', lwd = 3, lty = 2)
        
      }
    } else{
      if(!input$projection){
        plot(plotdata$Year, plotdata$LifeScore,
-            type = 'l', lwd = 2, col = 'green', xlim = c(2005, 2017), ylim = c(-10, 10),
-            main = "Cam's Utility Score", xlab = 'Year', ylab = 'Utility')
+            type = 'l', lwd = 3, col = 'blue', xlim = c(2005, 2017), ylim = c(-6, 6),
+            main = "Cam's Life Benefit Score", xlab = 'Year', ylab = 'Utility')
      } else{
        plot(plotdata$Year[plotdata$Year < 2050], plotdata$LifeScore[plotdata$Year < 2050],
-            type = 'l', lwd = 2, col = 'green', xlim = c(2005, 2050), ylim = c(-10, 50),
-            main = "Cam's Utility Score", xlab = 'Year', ylab = 'Utility')
-       lines(c(2017, 2050), c(plotdata$Lifescore[plotdata$Year == 2017], plotdata$Lifescore[plotdata$Year == 2017] + 45),
-             col = 'green', lwd = 2, lty = 2)
+            type = 'l', lwd = 3, col = 'blue', xlim = c(2005, 2050), ylim = c(-5, 50),
+            main = "Cam's Life Benefit Score", xlab = 'Year', ylab = 'Utility')
+       lines(c(2017, 2050), c(plotdata$LifeScore[plotdata$Year == 2017], plotdata$LifeScore[plotdata$Year == 2017] + 45),
+             col = 'blue', lwd = 3, lty = 2)
        
      }
      
    }
+   
+   abline(h = 0, lwd = 2, lty = 2, col = 'black')
     
-  })
+  }, height = 400, width = 600)
 })
